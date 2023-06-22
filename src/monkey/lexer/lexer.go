77766f701d1dd3cd107b7c0960ee0e0b72912267
Grimm33 <yandrick.alpha.grima@gmail.com>
2023-06-22 20:51:27 +0200

    Adding Identifier support

--- src/monkey/lexer/lexer.go
+++ src/monkey/lexer/lexer.go
@@ -28,6 +28,8 @@ func (l *Lexer) ReadChar() {
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhitespace()
+
 	switch l.ch {
 	case '=':
 		tok = NewToken(token.ASSIGN, l.ch)
@@ -48,12 +50,39 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.ReadIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else {
+			tok = NewToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.ReadChar()
 	return tok
 }
 
+func (l *Lexer) ReadIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.ReadChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\r' || l.ch == '\n' {
+		l.ReadChar()
+	}
+}
+
 func NewToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
